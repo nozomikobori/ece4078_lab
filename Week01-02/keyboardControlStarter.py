77df0655ac35d8a3352613bbde574017d14e85ec
nozomikobori <nozomi.persona@gmail.com>
2020-08-19 15:33:07 +1000

    The begining of Week3

--- Week01-02/keyboardControlStarter.py
+++ Week01-02/keyboardControlStarter.py
@@ -7,8 +7,10 @@ import numpy as np
 
 class Keyboard:
     # feel free to change the speed, or add keys to do so
+
     wheel_vel_forward = 100
-    wheel_vel_rotation = 20
+    # speed of the wheel
+    wheel_vel_rotation = 10
 
     def __init__(self, ppi=None):
         # storage for key presses
@@ -25,29 +27,66 @@ class Keyboard:
         print(key)
         # use arrow keys to drive, space key to stop
         # feel free to add more keys
+
         if key == Key.up:
+            self.directions[1] = False
             self.directions[0] = True
         elif key == Key.down:
+            self.directions[0] = False
             self.directions[1] = True
         elif key == Key.left:
+            self.directions[3] = False
             self.directions[2] = True
         elif key == Key.right:
+            self.directions[2] = False
+            self.directions[3] = True
+        elif key == Key.space:
+            self.directions = [False for _ in range(4)]
+            self.signal_stop = True
+        elif key == KeyCode.from_char('a'):
+            print("Speed up")
+            self.wheel_vel_forward += 10
+        elif key == KeyCode.from_char('d'):
+            print("Speed down")
+            self.wheel_vel_forward -= 10
+        
+        # Stop everything before next action 
+        '''
+        if key == Key.up:
+            self.directions = [False for _ in range(4)]
+            self.directions[0] = True
+        elif key == Key.down:
+            self.directions = [False for _ in range(4)]
+            self.directions[1] = True
+        elif key == Key.left:
+            self.directions = [False for _ in range(4)]
+            self.directions[2] = True
+        elif key == Key.right:
+            self.directions = [False for _ in range(4)]
             self.directions[3] = True
         elif key == Key.space:
             self.signal_stop = True
+        elif key == KeyCode.from_char('a'):
+            print("Speed up")
+            self.wheel_vel_forward += 10
+        elif key == KeyCode.from_char('d'):
+            print("Speed down")
+            self.wheel_vel_forward -= 10
+        '''
 
         self.send_drive_signal()
         
     def get_drive_signal(self):           
-        # translate the key presses into drive signals 
-        
+        # translate the key presses into drive signals   
         # compute drive_forward and drive_rotate using wheel_vel_forward and wheel_vel_rotation
-        # drive_forward = ???
-        # drive_rotate = ???
+        drive_forward = self.directions[0] * self.wheel_vel_forward - self.directions[1] * self.wheel_vel_forward 
+        drive_rotate = self.directions[2] * self.wheel_vel_rotation - self.directions[3] * self.wheel_vel_rotation 
 
         # translate drive_forward and drive_rotate into left_speed and right_speed
-        # left_speed = ???
-        # right_speed = ???
+        left_speed = drive_forward - drive_rotate
+        right_speed = drive_forward + drive_rotate
+        print(left_speed, right_speed)
+        print(self.directions)
 
         return left_speed, right_speed
     
@@ -92,6 +131,10 @@ if __name__ == "__main__":
 
         # feel free to add more GUI texts
         cv2.putText(resized, 'PenguinPi', (15, 50), font, font_scale, font_col, line_type)
+
+        cv2.putText(resized, "Left wheel velocity: "+str(L_Wvel), (15, 500), font, font_scale, font_col, line_type)
+        cv2.putText(resized, "Right wheel velocity: "+str(R_Wvel), (15, 600), font, font_scale, font_col, line_type)
+        
 
         cv2.imshow('video', resized)
         cv2.waitKey(1)
